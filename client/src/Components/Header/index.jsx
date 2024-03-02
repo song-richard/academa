74da62571cc1a01afe2daefd564ab33ee5a56dad
song-richard <rsong1993@gmail.com>
2024-03-02 14:50:18 -0800

    Wrapped Academa in Typography with flexGrow to ensure component expands to fill the available space on the left side of the AppBar with h6 heading. Added buttons for 'Login' and 'Signup'

--- client/src/Components/Header/index.jsx
+++ client/src/Components/Header/index.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 
 //MUI IMPORTS
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 export const Header = () => {
   return (
     <>
       <AppBar position='static'>
-        Test
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Academa
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Signup</Button>
+        </Toolbar>
       </AppBar>
     </>
   )
