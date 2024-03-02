7de04f11e6536944d2b071e388e448ebb77f9d34
song-richard <rsong1993@gmail.com>
2024-03-02 15:06:04 -0800

    Imported Grid, Card, CardContent from MUI. Nested (3) child Grids within (1) parent Grid to render Current Decks. Updated each deck title with h6 headings. Updated parent Grid spacing to '2' so there will be a spacing of 2 units between grid items inside the container

--- client/src/Components/Content/index.jsx
+++ client/src/Components/Content/index.jsx
@@ -9,6 +9,37 @@ export const Content = () => {
       <Typography variant="h5" gutterBottom>
         Current Decks
       </Typography>
+
+      <Grid container spacing={2}>
+
+        {/* CARD 1 */}
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">Deck 1</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* CARD 2 */}
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">Deck 2</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        {/* CARD 3 */}
+        <Grid item xs={4}>
+          <Card>
+            <CardContent>
+              <Typography variant="h6">Deck 3</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+
+      </Grid>
     </>
-  )
-}
+  );
+};
