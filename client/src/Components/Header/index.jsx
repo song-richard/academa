f204f8a31a73fb707396541196271be7030f5504
Richard Song <rsong1993@gmail.com>
2024-03-04 20:43:20 -0800

    Found AuthO bug. ReactDOM.render is triggering an error.

--- client/src/Components/Header/index.jsx
+++ client/src/Components/Header/index.jsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
 
+
+
 //MUI IMPORTS
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 
 export const Header = () => {
   const [showLogin, setShowLogin] = useState(false);
   
+  const handleLoginClick = () => {
+    setShowLogin(true);
+  };
+
+  const handleCloseLogin = () => {
+    setShowLogin(false);
+  };
 
   return (
     <>
@@ -14,7 +23,7 @@ export const Header = () => {
           <Typography variant="h6" sx={{ flexGrow: 1 }}>
             Academa
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" onClick={handleLoginClick}>Login</Button>
           <Button color="inherit">Signup</Button>
         </Toolbar>
       </AppBar>
