c41a954bbc72f2384438fa914cddba485cdd8a5b
Richard Song <rsong1993@gmail.com>
2024-03-05 12:47:34 -0800

    Imported useQuery hook to CardComponent

--- client/src/Components/Content/CardComponent.jsx
+++ client/src/Components/Content/CardComponent.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+
+//MUI Imports
 import { Typography, Card, CardContent } from '@mui/material';
+
+//Apollo Imports
+import { useQuery } from '@apollo/client';
 
 export default function CardComponent({ title }) {
     return (
