98eb0a0998906d0379091f29b3bbd9dc3f158181
Pak <pakapongm@gmail.com>
2024-03-04 17:08:33 -0800

    configure auth0 into entry point

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,20 +5,16 @@ import App from './App.jsx'
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-
-
-
-const root = createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
 <Auth0Provider
     domain={domain}
     clientId={clientId}
@@ -28,4 +24,5 @@ root.render(
   >
     <App />
   </Auth0Provider>,
+  document.getElementById('root')
 );
