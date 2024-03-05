095e9dec30774ae27c1f83d33435ed00e9a4f88b
Pak <pakapongm@gmail.com>
2024-03-04 17:28:41 -0800

    in login comp added authentication using ternary to check lon in

--- client/src/Components/Login/index.jsx
+++ client/src/Components/Login/index.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 
-export const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+const AuthenticationLogin = () => {
+  const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
+  return isAuthenticated ? (
+    <button onClick={() => logout({ returnTo: window.location.origin })}>
+      Log Out
+    </button>
+  ) : (
+    <button onClick={loginWithRedirect}>Log In</button>
+  );
+};
+
+export default AuthenticationLogin;
