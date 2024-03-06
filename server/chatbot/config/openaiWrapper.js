8cbc42a82cb8d36e26ed25bed0d55fe0324f3b52
Amanuel <amanueleman@gmail.com>
2024-03-05 23:29:14 -0500

    Added open AI configration

---
+++ server/chatbot/config/openaiWrapper.js
@@ -0,0 +1,24 @@
+const { OpenAI } = require("@langchain/openai");
+require("dotenv").config();
+
+const { StructuredOutputParser } = require("langchain/output_parsers");
+
+//// Creates and stores a wrapper for the OpenAI package along with basic configuration
+const model = new OpenAI({
+  openAIApikey: process.env.OPENAI_API_KEY,
+  temperature: 0,
+  model: "gpt-3.5-turbo",
+});
+
+console.log("model", model);
+
+// With a `StructuredOutputParser` we can define a schema for the output.
+const parser = StructuredOutputParser.fromNamesAndDescriptions({
+    code: "Javascript code that answers the user's question",
+    explanation: "detailed explanation of the example code provided",
+  });
+
+// Get the format instructions from the parser
+const formatInstructions = parser.getFormatInstructions();
+
+  module.exports = { model, formatInstructions };
