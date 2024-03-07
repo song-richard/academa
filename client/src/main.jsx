f835be7c1bc8f539fe69e974309008437b585688
Kaila <kailaamia@gmail.com>
2024-03-06 22:45:27 -0600

    Imported dependencies

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+
+import Home from './pages/Home';
 
 const root = createRoot(document.getElementById('root'));
 // const domain = import.meta.env.AUTH0_DOMAIN;
