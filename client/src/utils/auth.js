253756cbf7ec355f95aa76c16525a36a766ffb1a
Kaila <kailaamia@gmail.com>
2024-03-06 21:09:35 -0600

    Added logout authService and comments

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -2,10 +2,18 @@ import decode from 'jwt-decode';
 import { useAuth0 } from '@auth0/auth0-react';
 
 class AuthService {
-  login = (idToken) => {
-    localStorage.setItem('id_token', idToken);
-    window.location.assign('/');
-  };
+    login = (idToken) => {
+        // Saves user token to localStorage and reloads the application for logged in status to take effect
+        localStorage.setItem('id_token', idToken);
+        window.location.assign('/');
+    };
+    logout() {
+        // Clear user token and profile data from localStorage
+        localStorage.removeItem('id_token');
+        // this will reload the page and reset the state of the application
+        window.location.reload();
+    }
+
 }
 
 export default new AuthService();
