73b335487fd35d901ecbfc01e1a709b3a002b4ec
Kaila <kailaamia@gmail.com>
2024-03-07 15:18:02 -0600

    Uncommented bycrypt code in profile model

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -29,21 +29,21 @@ const profileSchema = new Schema({
 }, { toJSON: { virtuals: true }, id: false, }
 );
 
-//Uncomment this if we use passwod again
-// // set up pre-save middleware to create password
-// profileSchema.pre('save', async function (next) {
-//   if (this.isNew || this.isModified('password')) {
-//     const saltRounds = 10;
-//     this.password = await bcrypt.hash(this.password, saltRounds);
-//   }
 
-//   next();
-// });
+// set up pre-save middleware to create password
+profileSchema.pre('save', async function (next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
 
-// // compare the incoming password with the hashed password
-// profileSchema.methods.isCorrectPassword = async function (password) {
-//   return bcrypt.compare(password, this.password);
-// };
+  next();
+});
+
+// compare the incoming password with the hashed password
+profileSchema.methods.isCorrectPassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 const Profile = model('profile', profileSchema);
 
