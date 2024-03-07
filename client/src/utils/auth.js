73e03e129f5dcaf8926cb5c740c667f9f452c94a
Kaila <kailaamia@gmail.com>
2024-03-06 21:08:16 -0600

    Added login authService

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,2 +1,11 @@
 import decode from 'jwt-decode';
 import { useAuth0 } from '@auth0/auth0-react';
+
+class AuthService {
+  login = (idToken) => {
+    localStorage.setItem('id_token', idToken);
+    window.location.assign('/');
+  };
+}
+
+export default new AuthService();
