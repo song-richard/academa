03887cafad04617ee3168df940b9745cd2736686
Kaila <kailaamia@gmail.com>
2024-03-06 23:10:27 -0600

    Added studycardset route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,16 +2,21 @@ import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider} from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import Home from './pages/Home';
+import StudyCardSet from './pages/StudyCardSet';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
     children: [
-      { index: true, element: <Home />},
+      { index: true, element: <Home /> },
+      {
+        path: 'studyCardSet/:cardSetId',
+        element: <StudyCardSet />,
+      }
     ],
   }
 ]);
@@ -21,9 +26,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // const audience = import.meta.env.AUTH0_AUDIENCE;
 
 root.render(
-<Auth0Provider
-  domain = "dev-bh6v4jvlxjk663r7.us.auth0.com"
-  clientId = "yiM1pcbX4WoD4iFTBSRtfOlvMdt32HeQ"
+  <Auth0Provider
+    domain="dev-bh6v4jvlxjk663r7.us.auth0.com"
+    clientId="yiM1pcbX4WoD4iFTBSRtfOlvMdt32HeQ"
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
