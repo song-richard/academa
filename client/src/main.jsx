b3d5db0e5e85af56bcd937dd10830e0b8262fc5b
Kaila <kailaamia@gmail.com>
2024-03-06 22:53:09 -0600

    Removed extra import

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,6 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
 import Home from './pages/Home';
-import App from './App.jsx';
 
 const router = createBrowserRouter([
   {
