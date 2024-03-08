db6c2dc8bc3f1baf7893a162ecb8b402746d3143
Richard Song <rsong1993@gmail.com>
2024-03-08 15:21:38 -0800

    Added DeleteCardSet component logic

--- client/src/pages/DeleteCardSet.jsx
+++ client/src/pages/DeleteCardSet.jsx
@@ -4,8 +4,25 @@ import { DELETE_CARDSET } from '../utils/queries';
 
 import React from 'react'
 
-export const DeleteCardSet = () => {
-  return (
-    <div>DeleteCardSet</div>
-  )
-}
+const DeleteCardSet = ({ id }) => {
+    const [deleteCardSet, { error }] = useMutation(DELETE_CARDSET);
+  
+    const handleDelete = async () => {
+      try {
+        const { data } = await deleteCardSet({
+          variables: {
+            id: id
+          }
+        });
+        console.log('Flashcard set deleted:', data.deleteCardSet);
+      } catch (error) {
+        console.error('Error deleting flashcard set:', error);
+      }
+    };
+  
+    return (
+      <button onClick={handleDelete}>Delete Card Set</button>
+    );
+  };
+  
+  export default DeleteCardSet;
