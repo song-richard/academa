daa7c375cde04a054a818dcde1e17334062adab9
Richard Song <rsong1993@gmail.com>
2024-03-08 15:18:34 -0800

    Created DeleteCardSet function

--- client/src/pages/DeleteCardSet.jsx
+++ client/src/pages/DeleteCardSet.jsx
@@ -1,3 +1,11 @@
 import React from 'react';
 import { useMutation } from '@apollo/client';
 import { DELETE_CARDSET } from '../utils/queries';
+
+import React from 'react'
+
+export const DeleteCardSet = () => {
+  return (
+    <div>DeleteCardSet</div>
+  )
+}
