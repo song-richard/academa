fc793c6316db20cc2db652d7eadb9065c6760da9
Pak <pakapongm@gmail.com>
2024-03-08 20:56:41 -0800

    refactored and deleted auth0 code

--- client/src/Components/Header/index.jsx
+++ client/src/Components/Header/index.jsx
@@ -1,45 +1,34 @@
-import { useState } from 'react';
 //MUI IMPORTS
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-//auth0 hook
-import { useAuth0 } from '@auth0/auth0-react';
 import Auth from '../../utils/auth';
 
-const LoginButton = () => {
-  const { loginWithRedirect } = useAuth0();
-
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
-
-};
-
 const redirect = (route) => {
- window.location.assign(route);
+  window.location.assign(route);
 }
 
 export const Header = () => {
-  const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
       <AppBar position='static'>
         <Toolbar>
-          <Typography variant="h6" sx={{ flexGrow: 1 }} onClick={()=> redirect('/')}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }} onClick={() => redirect('/')}>
             Academa
           </Typography>
           {Auth.loggedIn() ? (
             <div>
-              <Button color="inherit" onClick={()=> redirect('./pages/createCardSet')}>Create Cards</Button>
-              <Button color="inherit" onClick={()=> redirect('./pages/generateAiCardsets')}>Create AI Cards</Button>
+              <Button color="inherit" onClick={() => redirect('./pages/createCardSet')}>Create Cards</Button>
+              <Button color="inherit" onClick={() => redirect('./pages/generateAiCardsets')}>Create AI Cards</Button>
               <Button color="inherit" onClick={Auth.logout}>Logout</Button>
             </div>
-          ): (
+          ) : (
             <div>
-              <Button color="inherit" onClick={()=> redirect('/login')}>Login</Button>
-              <Button color="inherit" onClick={()=> redirect('/signup')}>Signup</Button>
+              <Button color="inherit" onClick={() => redirect('/login')}>Login</Button>
+              <Button color="inherit" onClick={() => redirect('/signup')}>Signup</Button>
             </div>
           )}
         </Toolbar>
-    </AppBar >
+      </AppBar >
     </>
   )
 }
