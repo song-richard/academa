d5f47bf5e98fbb6be28b8c7c6b99ccf67f829a66
Pak <pakapongm@gmail.com>
2024-03-08 18:16:26 -0800

    handle the delete when user wants tp delete their card and console log. still need to add user confirmation

--- client/src/pages/StudyCardSet.jsx
+++ client/src/pages/StudyCardSet.jsx
@@ -1,10 +1,60 @@
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GET_CARDSET } from '../utils/queries';
+import { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { DELETE_CARDSET, UPDATE_CARDSET } from '../utils/mutations';
+
+
 
 const StudyCardSet = () => {
-    
+
+    // get the card set id from the url
+    const { cardSetId } = useParams();
+    const { loading, data } = useQuery(GET_CARDSET, {
+        variables: { id: cardSetId }
+    });
+
+    // get the cards from the card set
+    const cards = data?.cardSet.cards || [];
+    const [deleteCardSet, { error }] = useMutation(DELETE_CARDSET);
+    const [cardId, setCardId] = useState(0);
+    const [cardvalue, setCardValue] = useState(cards[cardId].term);
+
+    const handleDelete = async () => {
+        try {
+            // delete the card set from the database
+            // let user confirm that they want to delete the card set
+            // if the user confirms, delete the card set
+            // if the user cancels, do nothing
+            const { data } = await deleteCardSet({
+                variables: {
+                    id: cardSetId
+                }
+            });
+            console.log('Flashcard set deleted:', data.deleteCardSet);
+        } catch (error) {
+            console.error('Error deleting flashcard set:', error);
+        }
+    };
+    const handleUpdate = async () => {
+        try {
+            const { data } = await updateCardSet({
+                variables: {
+                    id: cardSetId
+                }
+            });
+            console.log('Flashcard set updated:', data.updateCardSet);
+        } catch (error) {
+            console.error('Error updating flashcard set:', error);
+        }
+    }
+
+
     return (
         <div>
             <h1>Study Cards (title of set will go here)</h1>
-
+            
             {/* create container component */}
             {/* get all the cards from the current set from the backend using a usequery */}
             {/* show only the first card in the set. just show the term */}
