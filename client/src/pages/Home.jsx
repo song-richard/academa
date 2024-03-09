839d46cf3222e3bd764b4906abd6da6de31c3922
Richard Song <rsong1993@gmail.com>
2024-03-08 16:41:03 -0800

    Styled Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -11,12 +11,14 @@ const Home = () => {
     //Home page if user is not logged in
     //TODO: Style this page
     return (
-        <div>
-            <h1>Welcome Academa</h1>
-            <h2>Academa is a platform that allows students to create and study flashcards</h2>
-            <button>Get Started</button>
+        <div className="max-w-lg mx-auto px-4 py-8 text-center">
+            <h1 className="text-4xl font-bold mb-4">Welcome to Academa</h1>
+            <h2 className="text-lg text-gray-700 mb-8">Academa is a platform that allows students to create and study flashcards</h2>
+            <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                Get Started
+            </button>
         </div>
-    )
+    );
 }
 
 export default Home;
