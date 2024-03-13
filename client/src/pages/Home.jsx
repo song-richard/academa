97cb7e9971699bb6d3977be0b56bec4dfd054423
Kaila <kailaamia@gmail.com>
2024-03-13 16:36:50 -0500

    Added link to login on get started button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import {Content} from '../Components/Content';
+import { Content } from '../Components/Content';
 import Auth from '../utils/auth';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-    
+
     //Home page if user is logged in
-    if(Auth.loggedIn()){
+    if (Auth.loggedIn()) {
         return (<Content />);
     }
 
@@ -14,9 +15,12 @@ const Home = () => {
         <div className="max-w-lg mx-auto px-4 py-8 text-center">
             <h1 className="text-4xl font-bold mb-4">Welcome to Academa</h1>
             <h2 className="text-lg text-gray-700 mb-8">Academa is a platform that allows students to create and study flashcards</h2>
-            <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100">
-                Get Started
-            </button>
+            <Link to='/login'>
+                <button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-md shadow-md transition duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+                    Get Started
+                </button>
+            </Link>
+
         </div>
     );
 }
