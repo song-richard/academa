fd2a923d70b8bcdead55704f96f02f49f443837d
Pak <pakapongm@gmail.com>
2024-03-13 17:42:16 -0700

    handler function for modal, and imports

--- client/src/pages/DeleteCardSet.jsx
+++ client/src/pages/DeleteCardSet.jsx
@@ -1,13 +1,29 @@
-import { useMutation } from '@apollo/client';
+import { from, useMutation } from '@apollo/client';
+import { useState } from 'react';
 import { DELETE_CARDSET } from '../utils/mutations';
 import Auth from '../utils/auth';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
+Modal.setAppElement('#root');
 
 const DeleteCardSet = ({ id }) => {
   const [deleteCardSet, { error }] = useMutation(DELETE_CARDSET);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   const handleDelete = async () => {
     try {
-      const confirmDelete = window.confirm('Deleting this card is permanent. Are you sure you want to delete it?');
+      openModal();
       if (confirmDelete) {
         const { data } = await deleteCardSet({
           variables: {
@@ -20,6 +36,18 @@ const DeleteCardSet = ({ id }) => {
     } catch (error) {
       console.error('Error deleting flashcard set:', error);
     }
+
+
+    const handleModal = () => {
+
+      const openModal = () => {
+        setIsOpen(true);
+      }
+      const closeModal = () => {
+        setIsOpen(false);
+      }
+
+    };
   };
 
   if (Auth.loggedIn()) {
